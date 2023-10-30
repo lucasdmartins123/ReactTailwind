6bca59945cedadee4c37a464b10bc6743b25b015
Lucas Martins <lucasdallmartims@gmail.com>
2023-10-30 16:22:42 -0300

    first commit

---
+++ reactTailwind/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import { MoonIcon } from "@heroicons/react/24/solid";
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
+
+const Header = ({ user }) => {
+  return (
+    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <span className="text-gray-100">Olá, {user?.name || "Usuario"}</span>
+      <h1>Alura Newsletter</h1>
+      <ToggleTheme />
+    </div>
+  );
+};
+
+export default Header;
